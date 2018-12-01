69279ace38c6ce2a2ee67617772e490a36332a1c
“e <l>
2018-12-01 21:29:30 +0800

    sjw

--- src/view/accounts/finance.vue
+++ src/view/accounts/finance.vue
@@ -28,11 +28,10 @@
                    <p class="flex1 tc">{{item.cny_price}}</p>
                    <!-- <p class="flex1 tc">{{item.lock_position}}</p> -->
                    <p class="flex1 tc operation">
-                       <span v-if="item.currency_name == 'BRCS'||item.currency_name == 'UMP'" @click="noopen" class="no_open">充币</span>
-                       <span v-if="item.currency_name != 'BRCS'&&item.currency_name != 'UMP'" @click="excharge(index,item.currency)">充币</span>
-                       <span v-if="item.currency_name == 'BRCS'||item.currency_name == 'UMP'" @click="noopen" class="no_open">提币</span>
-                       <span @click="withdraw(index,item.currency,item.currency_name)" v-if="item.currency_name != 'BRCS'&&item.currency_name != 'UMP'">提币</span>
-                       <!-- <span @click="exchange">兑换</span> -->
+                       <span v-if="item.is_lever==0" @click="noopen" class="no_open">充币</span>
+                       <span v-if="item.is_lever==1" @click="excharge(index,item.currency)">充币</span>
+                       <span v-if="item.is_lever==0" @click="noopen" class="no_open">提币</span>
+                       <span v-if="item.is_lever==1" @click="withdraw(index,item.currency,item.currency_name)">提币</span>
                        <span @click="rec(index,item.currency)">记录</span>
                    </p>
                    </div>
