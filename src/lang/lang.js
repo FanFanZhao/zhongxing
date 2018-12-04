86e49a9b0d4878fced6eb1fde0e2d569ad5fe403
wzg <codcoe@foxmail.com>
2018-12-04 17:43:24 +0800

    lang

--- src/lang/lang.js
+++ src/lang/lang.js
@@ -3,9 +3,13 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 import zh from './zh'
 import en from './en'
-let locale = window.localStorage.getItem('locale') || 'zh';
+let locale = window.localStorage.getItem('locale') || '';
+if(locale == ''){
+  locale = 'zh';
+  window.localStorage.setItem('locale','zh');
+}
 export default new VueI18n({
-  locale:'en',
+  locale:locale,
   messages:{
     zh:zh,
     en:en
